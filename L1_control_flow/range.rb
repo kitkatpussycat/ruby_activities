1063f8ebbd0fcf4b66ded2a8b21f802601a8ffb9
kitkatpussycat <keith.m.pineda@gmail.com>
2022-01-19 22:58:01 +0800

    second assignment

--- L1_control_flow/range.rb
+++ L1_control_flow/range.rb
@@ -1,6 +1,8 @@
 print "enter a number between 0 and 100"
 input_number = gets.chomp.to_i
 
+#first solution
+
 if input_number <= 50 && input_number >= 0 
 puts "number is between 0-50"
 
@@ -14,3 +16,20 @@ else
     puts "invalid number"
 
 end
+
+#Second solution
+
+if (input_number < 0)
+    puts "invalid number"
+end
+if input_number in 0..50
+    puts "number is between 0-50"
+end
+if input_number in 51..100
+    puts "number is between 51-100"
+end
+if (input_number > 100)
+    puts "number is greater than 100"
+end
+
+
