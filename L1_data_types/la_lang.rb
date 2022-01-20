86d1573621c3c81d43f18a331834c3e7c7d0501e
kitkatpussycat <keith.m.pineda@gmail.com>
2022-01-20 20:57:10 +0800

    setter getter assignment

--- L1_data_types/la_lang.rb
+++ L1_data_types/la_lang.rb
@@ -1,4 +1,28 @@
-arr = [1,3,5,7,9,11]
-number = 3
+class Classmate
+    def initialize(name, type, gender)
+        @name = name
+        @type = type
+        @gender = gender
+    end
 
-arr.include?number
+    def greetings
+        puts "Hello #{@name}!"
+    end
+
+    #need ito para makuha ang value 
+    #kapag classmate_one.name nalang sabihin mo
+    attr_reader :name
+
+    #need naman ito para mapalitan yung value ng name.. magwrite ka
+    attr_writer :type
+
+    #pwedeng maaccess., pwedeng machange.. 
+    #parang pinagsamang read and write
+    attr_accessor :gender
+
+end
+
+classmate_one = Classmate.new("margie", "geek", "female")
+classmate_one.greetings
+puts classmate_one.name
+puts classmate_one.type
